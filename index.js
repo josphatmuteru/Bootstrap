bd49c35c63fa52511b36b476852833611561b956
Josphat Muteru <58342288+josphatmuteru@users.noreply.github.com>
2022-07-07 13:19:43 +0300

    Implemented url variable parsing

--- index.js
+++ index.js
@@ -66,11 +66,12 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
+  const { query, pathname } = url.parse(req.url, true);
+  // const pathname = req.url;
   // console.log(req.url);
 
   //Overview page
-  if (pathName === "/" || pathName === "/overview") {
+  if (pathname === "/" || pathname === "/overview") {
     res.writeHead(200, { "Content-type": "text/html" });
 
     const cardsHtml = dataObj
@@ -82,11 +83,15 @@ const server = http.createServer((req, res) => {
     res.end(output);
 
     //Product page
-  } else if (pathName === "/product") {
-    res.end("This is the PRODUCT");
+  } else if (pathname === "/product") {
+    res.writeHead(200, { "Content-type": "text/html" });
+
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
 
     //API
-  } else if (pathName === "/api") {
+  } else if (pathname === "/api") {
     fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, data) => {
       res.writeHead(200, { "Content-type": "application/JSON" });
       res.end(data);
